4fe7e6f3f5daec7717b88e4f484bdb872ef7aacd
Neil Smith <neil.git@njae.me.uk>
2014-01-07 23:52:49 +0000

    Made format_spec more exacting

--- spec/requests/format_spec.rb
+++ spec/requests/format_spec.rb
@@ -5,51 +5,61 @@ describe "format" do
   it "serves index as html by default" do
     get_via_redirect '/index'
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves index.html as html" do
     get_via_redirect '/index.html'
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves index.rss as rss" do
     get_via_redirect '/index.rss'
     expect(response.header['Content-Type']).to include('application/rss+xml')
+    expect(response.header['Content-Type']).not_to include('text/html')
   end
   
   it "serves index as html with the accept header" do
     get_via_redirect '/index', {}, {'Accept' => 'text/html'}
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves index as rss with the accept header" do
     get_via_redirect '/index', {}, {'Accept' => 'application/rss+xml'}
     expect(response.header['Content-Type']).to include('application/rss+xml')
+    expect(response.header['Content-Type']).not_to include('text/html')
   end
 
   it "serves feed as html by default" do
     get_via_redirect '/other_test_feed'
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves feed.html as html" do
     get_via_redirect '/other_test_feed.html'
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves feed.rss as rss" do
     get_via_redirect '/other_test_feed.rss'
     expect(response.header['Content-Type']).to include('application/rss+xml')
+    expect(response.header['Content-Type']).not_to include('text/html')
   end
   
   it "serves feed as html with the accept header" do
     get_via_redirect '/other_test_feed', {}, {'Accept' => 'text/html'}
     expect(response.header['Content-Type']).to include('text/html')
+    expect(response.header['Content-Type']).not_to include('application/rss+xml')
   end
 
   it "serves feed as rss with the accept header" do
     get_via_redirect '/other_test_feed', {}, {'Accept' => 'application/rss+xml'}
     expect(response.header['Content-Type']).to include('application/rss+xml')
+    expect(response.header['Content-Type']).not_to include('text/html')
   end
 end
 
