34303fd9cbaa54504acea104ce263a066b4de080
Neil Smith <neil.git@njae.me.uk>
2014-01-13 11:08:18 +0000

    Updated deploy-sample.rb to link the secret_token.rb file on deployment

--- config/deploy-sample.rb
+++ config/deploy-sample.rb
@@ -2,19 +2,22 @@ set :application, 'my_app_name'
 set :repo_url, 'git@example.com:me/my_repo.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, 'master'
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :default_stage, 'production'
 
-# set :format, :pretty
+set :deploy_to, '/var/www/my_app'
+set :scm, :git
+
+set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
 
 namespace :deploy do
 
@@ -22,7 +25,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
